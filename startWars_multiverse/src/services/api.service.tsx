8d30e4679bef7fc96b2310de55ea267f693b6b94
edumixer <edu.cena.wf@gmail.com>
2023-10-10 12:49:48 -0300

    chore(api): update API service and add methods

--- startWars_multiverse/src/services/api.service.tsx
+++ startWars_multiverse/src/services/api.service.tsx
@@ -9,7 +9,7 @@ const starWarsApi = axios.create({
   },
 });
 
-async function fetchData() {
+async function fetchAllCharacters() {
   try {
     const response = await starWarsApi.get("api/people/");
     return response.data;
@@ -18,4 +18,24 @@ async function fetchData() {
   }
 }
 
-export default {fetchData};
+async function fetchAllPlanets() {
+  try {
+    const response = await starWarsApi.get("api/planets/1");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fetchCharacterByName(name) {
+  try {
+    const response = await starWarsApi.get(`api/people/?search=${name}`);
+    console.log("Chamada API fetchByName: ", response.data)
+    const character = response.data.results[0];
+    return character;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default {fetchAllCharacters, fetchAllPlanets, fetchCharacterByName};
